a5f17acf842b1a56d6bd4175c7a3d8b0e04dd7c6
claragimenojesus <79370643+claragimenojesus@users.noreply.github.com>
2025-12-12 16:07:37 +0000

    Update and rename 02_aggregate_jules_output.sh to 02_combine_jules_output_OGGM.sh

---
+++ workflow/jules_postprocessing/02_combine_jules_output_OGGM.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+cd $PBS_O_WORKDIR
+
+export NBS=""
+
+CONFIG_FILE=$(ls -dv rahu-config_files_rcp45/* | head -n $PBS_ARRAY_INDEX | tail -n 1)
+MOSART_FILE=$(ls -dv config_rahu_files_rcp45/mosart* | head -n $PBS_ARRAY_INDEX | tail -n 1)
+CONFIG_FILE_METEO=$(ls -dv rahu-config_files_rcp45_meteo/* | head -n $PBS_ARRAY_INDEX | tail -n 1)
+
+export SCENARIO_SH=$(ls -dv  "$EPHEMERAL/"JULES_OGGM/rcp45/* | head -n $PBS_ARRAY_INDEX | tail -n 1)
+export SCENARIO_OUTPUT_SH=$(ls -dv "$EPHEMERAL/"jules-output/rcp45/* | head -n $PBS_ARRAY_INDEX | tail -n 1)
+export HIST_OUTPUT_SH=$(ls -dv "$HOME/"project/rahu/jules-output/u-cz655)
+export SCENARIO=$(basename $SCENARIO_SH)
+export RC="rcp45"
+
+export SRC_DIR=$(pwd)/../src
+echo "Coupling with JULES-OGGM"
+python ${SRC_DIR}/coupling_pt1.py
+
+cd $SCENARIO_SH
+cdo -yearadd -gtc,100000000 -inttime,2000-01-01,00:00:00,1day glacier_area_yyyy.nc glacier_area_yyyy.nc glacier_area_daily.nc
+cdo -yearadd -gtc,100000000 -inttime,2000-01-01,00:00:00,1day glacier_perc_per_gridcell.nc glacier_perc_per_gridcell.nc glacier_perc_daily.nc
+
+# then remove last day of data
+cdo -delete,date="2100-01-01T00:00:00" glacier_area_daily.nc glacier_area_daily_v2.nc
+cdo -delete,date="2100-01-01T00:00:00" glacier_perc_daily.nc glacier_perc_daily_v2.nc
+
+# also remove files not needed anymore
+rm glacier_area_yyyy.nc
+rm glacier_area_yyyy_v2.nc
+rm glacier_perc_per_gridcell.nc
+rm glacier_perc_per_gridcell_v2.nc
+
+cd $PBS_O_WORKDIR
+
+python ${SRC_DIR}/coupling_pt2.py
+
+cd $SCENARIO_SH
+# Delete files not needed
+rm glacier_area_daily.nc
+rm glacier_area_daily_v2.nc
+rm glacier_perc_daily.nc
+rm glacier_perc_daily_v2.nc
+rm rain_daily_kgm2s.nc
