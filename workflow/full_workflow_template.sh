32f11b983d32f34864628da94349e02c4efdb474
claragimenojesus <79370643+claragimenojesus@users.noreply.github.com>
2025-12-15 14:35:36 +0000

    Update full_workflow_template.sh

--- workflow/full_workflow_template.sh
+++ workflow/full_workflow_template.sh
@@ -78,3 +78,35 @@ echo "Mosart input complete"
 echo "Running Mosart"
 NUMBA_NUM_THREADS=32 ./$MOSART_FILE
 echo "Finished routing with Mosart"
+
+#!/bin/bash
+#PBS -l select=1:ncpus=20:mem=20gb
+#PBS -l walltime=03:00:00
+
+#module load anaconda3/personal
+#source activate mosart
+eval "$(~/anaconda3/bin/conda shell.bash hook)"
+source activate mosart
+
+cd $PBS_O_WORKDIR
+
+export INPUT_FILENAME="coupled_scpar_runoff.nc"
+export MOSART_INPUT=$(ls -dv "$EPHEMERAL/"mosart-input/rcp45/* | head -n 2 | tail -n 1)
+export MOSART_OUTPUT=$(ls -dv "$EPHEMERAL/"mosart-output/rcp45/* | head -n 2 | tail -n 1)
+export RCP_DIR="$EPHEMERAL/mosart-output/rcp45"
+export ANALYSIS_DIR="$HOME/analysis_files"
+export RC="rcp45"
+export SCENARIO_SH=$(ls -dv "$EPHEMERAL/"jules-output/rcp45/* | head -n 2 | tail -n 1)
+export SC=$(basename $SCENARIO_SH)
+
+export NBS=""
+echo "Extracting simulated routed flows for scenario $NBS"
+eval "$(~/anaconda3/bin/conda shell.bash hook)"
+source activate mosart
+
+export SRC_DIR=$(pwd)/src
+#conda activate mosart
+python ${SRC_DIR}/uh.py
+python ${SRC_DIR}/extract-qsim.py
+python ${SRC_DIR}/merged_qsim_qdeep.py
+
