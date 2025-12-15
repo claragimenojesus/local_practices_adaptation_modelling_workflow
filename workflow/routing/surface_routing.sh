0629550cbc5ab898ce0e045f4e9cf40d3bad3b93
claragimenojesus <79370643+claragimenojesus@users.noreply.github.com>
2025-12-15 14:26:41 +0000

    Update surface_routing.sh

--- workflow/routing/surface_routing.sh
+++ workflow/routing/surface_routing.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda deactivate
+
+export SRC_DIR=$(pwd)/../src
+conda activate mosart
+python ${SRC_DIR}/mosart-rahu.py # File mosart-rahu needs to be adapted for each scenario to point to the correct MOSART configuration file
+conda deactivate
